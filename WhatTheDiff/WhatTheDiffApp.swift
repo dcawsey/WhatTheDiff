9f89ff91874402c3c1a411eedc5de8ec4f21dd19
Dylan Cawsey <dylancawsey@gmail.com>
2023-08-23 21:05:51 +1000

    Initial Commit

---
+++ WhatTheDiff/WhatTheDiffApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatTheDiffApp.swift
+//  WhatTheDiff
+//
+//  Created by Dylan Cawsey on 23/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct WhatTheDiffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
